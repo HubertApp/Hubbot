a45f319ab8972d78796e74afc93bf601984ee243
Noe <noeziadi@outlook.com>
2025-03-26 08:45:05 +0100

    Ajout d'un ping au serveur

--- index.js
+++ index.js
@@ -49,6 +49,34 @@ app.post('/github-webhook', (req, res) => {
     res.status(200).send("OK");
 });
 
+const https = require('https');
+
+exports.handler = async (event, context) => {
+ const url = process.env.RENDER_URL;
+
+ return new Promise((resolve, reject) => {
+   const req = https.get(url, (res) => {
+     if (res.statusCode === 200) {
+       resolve({
+         statusCode: 200,
+         body: 'Server pinged successfully',
+       });
+     } else {
+       reject(
+         new Error(`Server ping failed with status code: ${res.statusCode}`)
+       );
+     }
+   });
+
+   req.on('error', (error) => {
+     reject(error);
+   });
+
+   req.end();
+ });
+};
+
+
 // Lorsque le bot reçoit un message
 client.on('messageCreate', (message) => {
     // Ignorer les messages du bot lui-même
